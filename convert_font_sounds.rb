75a82c4872f8b6001ead8c776e277b0249005176
Ash McKenzie <ash@the-rebellion.net>
2016-04-11 23:04:02 +1000

    Rogue!

--- convert_font_sounds.rb
+++ convert_font_sounds.rb
@@ -4,13 +4,14 @@ require 'pry-byebug'
 require 'pathname'
 require 'find'
 
+DEBUG                     = true
 OUTPUT_BASE               = "/Volumes/8GB"
 ORIGINAL_FONTS_BASE       = "/Users/ash/Dropbox/saber/Sounds/all"
 VALID_SOUND_FONTS_MAPPING = {
   [ /lockup/, /lock_up/ ]               => 'lockup',
   [ /idle/, /hum/ ]                     => 'idle',
-  [ /poweron/, /power_on/ ]             => 'poweron',
-  [ /poweroff/, /power_off/ ]           => 'poweroff',
+  [ /poweron/, /power_on/, /pwron/ ]             => 'poweron',
+  [ /poweroff/, /power_off/, /pwroff/ ]           => 'poweroff',
   [ /swing(\d{,1})/, /motion(\d{,1})/ ] => 'swing\1',
   [ /clash(\d{,1})/, /impact(\d{,1})/ ] => 'clash\1'
 }
@@ -19,7 +20,7 @@ def convert(source_dir, destination_dir)
   source_dir = "#{ORIGINAL_FONTS_BASE}/#{source_dir}"
   destination_dir = "#{OUTPUT_BASE}/#{destination_dir}"
 
-  `mkdir -p "#{destination_dir}"`
+  `mkdir -p "#{destination_dir}"` unless DEBUG
 
   sounds = Find.find(source_dir).select { |x| x.match(/.wav$/) }.select do |x|
     new_filename = nil
@@ -41,7 +42,7 @@ def convert(source_dir, destination_dir)
 
     command = "./sox_prep.sh #{file.to_s} #{new_file.to_s}"
     puts command
-    `#{command}`
+    `#{command}` unless DEBUG
   end
 end
 
@@ -54,3 +55,4 @@ convert('StarKiller', 'fonts/star_killer')
 convert('Unstable-Tsuarbisu', 'fonts/unstable')
 convert('Light_Meat', 'fonts/light_meat')
 convert('Dark_Meat', 'fonts/dark_meat')
+convert('Rogue', 'fonts/rogue')
